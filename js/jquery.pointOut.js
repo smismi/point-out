9a40438822642735c4ed37b65718c93a7bb37a39
smismi <smirnov.ru@gmail.com>
2014-02-24 17:39:57 +0400

    init

---
+++ js/jquery.pointOut.js
@@ -0,0 +1,23 @@
+(function ($) {
+
+
+	//инициализацичя плагина
+
+
+	$.fn.pointOut = function (options) {
+
+		var settings = $.extend({
+		 	script: [],
+			delay: 1000,
+			flag1: false
+		}, options);
+
+
+		debugger;
+	}
+
+
+
+
+
+})(jQuery);
