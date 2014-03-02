a9c82d9aafafbd404f5e6efdf72a8e160ec0b61c
smismi <smirnov.ru@gmail.com>
2014-03-02 12:52:57 +0400

    easing

--- js/jquery.pointOut.js
+++ js/jquery.pointOut.js
@@ -13,16 +13,84 @@
 		}, options);
 
 
+		var script = settings.script;
+		var delay = settings.delay;
+
+
+
+//TODO: 1. auto scroll
+//		2. svg-animation
+//		3. blur
+//      4. start + autostart
+
+
+//		foreach in array
+
+
+
+		var blump = new Blump();
+
+		$('body').append(blump);
+
+		script.map(runOnce);
 
 
 
 
-//
+
+
+		function runOnce(obj) {
+
+
+
+//			collect pos and dimensions of el
 
 
 
 
-		debugger;
+
+			_pos = {
+				x: 0,
+				y: 0,
+				w: 0,
+				h: 0
+			}
+
+
+			blump.animate({
+					top: _pos.y,
+					left: _pos.x
+				},
+				{
+					duration: delay,
+					specialEasing: {
+						top: "easeInQuad",
+						left: "easeInQuad"
+					}
+				}
+			);
+
+
+			//service output
+			console.log(obj.el, obj.pos)
+		}
+
+
+
+
+
+		function Blump() {
+
+			_el = $("<div>").addClass("blump");
+
+
+			return _el;
+
+
+		}
+
+
+
 	}
 
 
